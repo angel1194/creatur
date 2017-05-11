005b93034008af951de692773ae7acdaa8c7cb3c
Antonio Ortega <aortega@creatuviaje.com>
2017-05-11 15:06:15 -0500

    Login page finished

--- app/components/LoginContainer/index.js
+++ app/components/LoginContainer/index.js
@@ -1,65 +1,142 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled,{ css } from 'styled-components';
 import { Container } from 'semantic-ui-react';
+import { Link } from 'react-router'
+
+const ContainerL = styled(Container)`
+  display:flex !important;
+  justify-content:center;
+`;
 
 const LoginBody = styled.div`
   display:flex;
   flex-direction:column;
-  background-color:red;
+  flex-wrap:wrap;
   width:500px;
-  height:320px;
-  border: 2px solid #ecf0f1;
+  height:300px;
+  border: 1px solid #ccc
 `;
 
 const LoginHead = styled.div`
-  width:100%;
-  height:70px;
-  display:flex
-  flex-wrap:wrap;
+  display:flex;
+  justify-content:center;
   flex-direction:column;
+  flex-wrap:wrap;
   align-items:center;
+  width:100%;
+  height:60px;
   background-color: #2b5bac;
   color: #FFFFFF;
 `;
 
 const H3Login = styled.h3`
   color:white;
-  margin-left:-300px;
+  width:100%;
+  padding-left:70px;
   margin-bottom:5px;
 `;
 
 const Line = styled.div`
-  width:80%;
-  border: 1px solid white;
+  width:85%;
+  height:1px;
+  border-radius:2px;
+  border:1px solid #70a0ec;
 `;
 
 const LoginMain = styled.div`
-
+  width:100%;
+  height:55%;
+  padding:20px 40px;
 `;
 
-const Div = styled.div`
+const ContainerInput = styled.div`
+  width:100%;
+  height:40px;
   display:flex;
-  justify-content:center
+  flex-direction:row;
+  flex-wrap:nowrap;
+  box-shadow: inset 15px 3px 2px  #eaeaea;
+  border:1px solid #ccc;
+  margin-bottom:10px;
 `;
 
+const IconInput = styled.div`
+  width:40px;
+  height:39px;
+  background-color:#87b672;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  font-size:20px;
+  color:white;
+`;
+
+const Input = styled.input`
+  width:100%;
+  padding-left:10px;
+`;
+
+const Label = styled.label`
+  font-size:16px;
+  color:#2b5bac;
+`;
+
+const LoginFooter = styled.div`
+  width:100%;
+  height:24.8%;
+  display:flex;
+  flex-direction:row;
+  align-items:flex-end;
+  justify-content:space-between;
+  padding:20px 40px;
+`;
+
+const Button = styled.button`
+  background-color:#87b672;
+  padding:10px 50px;
+  color:white;
+  margin-left:20px;
+`;
+
+const LinkP = styled(Link)`
+  color:#b2b2b2 ;
+  text-decoration:underline;
+`;
 
 function LoginContainer() {
   return (
-    <Container>
-      <Div>
+    <ContainerL>
         <LoginBody>
           <LoginHead>
             <H3Login>Ingresa al Sistema</H3Login>
             <Line/>
           </LoginHead>
+          <LoginMain>
+            <Label htmlFor='mail'>Mail</Label>
+            <ContainerInput>
+              <IconInput>
+                <i className='fa fa-envelope'></i>
+              </IconInput>
+              <Input type='email' id='mail' placeholder='tucorreo@aqui.com'/>
+            </ContainerInput>
+            <Label htmlFor='password'>Contrasena</Label>
+            <ContainerInput>
+              <IconInput>
+                <i className='fa fa-lock'></i>
+              </IconInput>
+              <Input type='password' id='password'/>
+            </ContainerInput>
+          </LoginMain>
+          <LoginFooter>
+            <LinkP to=''>Olvidaste tu contrasena?</LinkP>
+            <Button>INGRESAR</Button>
+          </LoginFooter>
         </LoginBody>
-      </Div>
-    </Container>
+    </ContainerL>
   );
 }
 
 LoginContainer.propTypes = {
-
 };
 
 export default LoginContainer;
