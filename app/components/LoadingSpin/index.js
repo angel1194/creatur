c4b931152fb9562a3be01e7087ccf400feb1e323
Antonio Ortega <aortega@creatuviaje.com>
2017-05-30 12:30:19 -0500

    Change loadSpin

--- app/components/LoadingSpin/index.js
+++ app/components/LoadingSpin/index.js
@@ -14,7 +14,7 @@ function LoadingSpin(props) {
     if(loading) {
       return (
         <div className='divSpin'>
-          <i className="fa fa-cog fa-spin fa-3x fa-fw"></i>
+          <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
           <span className="sr-only">Loading...</span>
         </div>
       )
