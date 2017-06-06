a0c96b45674e6156f0d6d838f53b227b2c3b2591
Acopa123 <jose_acopa@hotmail.com>
2017-06-06 18:02:50 -0500

    Add Month on FormSearchHotel

--- app/components/ReserHotel/FormSearchHotel/index.js
+++ app/components/ReserHotel/FormSearchHotel/index.js
@@ -4,8 +4,11 @@
 *
 */
 
-import React from 'react';
+import React, {propTypes} from 'react';
 import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 import {Form, Grid, Input, Button, Header, Checkbox, Divider} from 'semantic-ui-react'
 import FormAutocomplete from '../../../components/FormAutocomplete'
 import { Link } from 'react-router'
@@ -16,81 +19,103 @@ import Cama from './cama.png';
 //   width: 400px;
 //   height: 300px;
 // `
+class FormSearchHotel extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment(),
+      endDate:moment().add(1, "days"),
+    };
+    this.handleChange = this.handleChange.bind(this)
+    this.handleChangeReturn = this.handleChangeReturn.bind(this)
+  }
 
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+  handleChangeReturn(date) {
+    this.setState({
+      endDate: date
+    })
+  }
 
-function FormSearchHotel(props) {
-  return (
-    <div id="inputSearchDisplay">
-      <div className='ui form' >
-        <Header as='h1' className='titleForm'>{props.title}</Header>
-        <Grid>
-          <div className='containerFormHotel'>
-            <FormAutocomplete placeholder='Destino, hotel, lugar de interés'/>
-            <div className='searchMap'>
-              <div>
-                <a id='search' href=''>
-                  <i className='fa fa-search fa-lg' aria-hidden='true'>
-                  </i> Buscar en Mapa
-                </a>
+  render() {
+    return (
+      <div id="inputSearchDisplay">
+        <div className='ui form' >
+          <Header as='h1' className='titleForm'>Bienvenido</Header>
+          <Grid>
+            <div className='containerFormHotel'>
+              <FormAutocomplete placeholder='Destino, hotel, lugar de interés'/>
+              <div className='searchMap'>
+                <div>
+                  <a id='search' href=''>
+                    <i className='fa fa-search fa-lg' aria-hidden='true'>
+                    </i> Buscar en Mapa
+                  </a>
+                </div>
               </div>
-            </div>
-            <div className="dad-dates">
-              <div className="dates">
-                <label className="fecha">Fecha de llegada</label>
-                <ul>
-                  <li><i className="fa fa-calendar fa-lg" aria-hidden="true"></i></li>
-                  <li><input type="text" placeholder="12/Ene/2016"/></li>
-                </ul>
-                <label className="fecha">Fecha de salida</label>
-                <ul>
-                  <li><i className="fa fa-calendar fa-lg" aria-hidden="true"></i></li>
-                  <li><input type="text" placeholder="12/Ene/2016"/></li>
-                </ul>
+              <div className="dad-dates">
+                <div className="dates">
+                  <label className="fecha">Fecha de llegada</label>
+                  <ul>
+                    <li><i className="fa fa-calendar fa-lg" aria-hidden="true"></i></li>
+                    <li><DatePicker selected={this.state.startDate} onChange={this.handleChange} minDate={moment()}/></li>
+
+                  </ul>
+                  <label className="fecha">Fecha de salida</label>
+                  <ul>
+                    <li><i className="fa fa-calendar fa-lg" aria-hidden="true"></i></li>
+                    <li><DatePicker selected={this.state.endDate} onChange={this.handleChangeReturn} minDate={moment(this.state.startDate)}/></li>
+                  </ul>
+                </div>
+                <div className="bed">
+                  <img src={Cama}/>
+                  <h4>4 noches</h4>
+                </div>
               </div>
-              <div className="bed">
-                <img src={Cama}/>
-                <h4>4 noches</h4>
+              <div className="dad-reserv">
+                <div className="child">
+                  <label htmlFor="">Habitaciones</label>
+                  <input className="inputs" type="number" placeholder="10"/>
+                </div>
+                <div className="child">
+                  <label htmlFor="">Adultos</label>
+                  <input className="inputs" type="number" placeholder="10"/>
+                </div>
+                <div className="child">
+                  <label htmlFor="">Ninos (0-7)</label>
+                  <input className="inputs" type="number" placeholder="10"/>
+                </div>
               </div>
-            </div>
-            <div className="dad-reserv">
-              <div className="child">
-                <label htmlFor="">Habitaciones</label>
-                <input className="inputs" type="number" placeholder="10"/>
+              <div className="edad">
+                <p>Edad de los menores</p>
               </div>
-              <div className="child">
-                <label htmlFor="">Adultos</label>
-                <input className="inputs" type="number" placeholder="10"/>
+              <div className="section-dad">
+                <div className="section-child">
+                  <label htmlFor="">Menor 1</label>
+                  <input className="inputs" type="number" placeholder="10"/>
+                </div>
+                <div className="section-child">
+                  <label htmlFor="">Menor 2</label>
+                  <input className="inputs" type="number" placeholder="10"/>
+                </div>
               </div>
-              <div className="child">
-                <label htmlFor="">Ninos (0-7)</label>
-                <input className="inputs" type="number" placeholder="10"/>
+              <div className="link">
+                <i className="fa fa-plus-circle fa-lg" aria-hidden="true"></i>
+                <a href="#">Añadir otra habitación</a>
               </div>
-            </div>
-            <div className="edad">
-              <p>Edad de los menores</p>
-            </div>
-            <div className="section-dad">
-              <div className="section-child">
-                <label htmlFor="">Menor 1</label>
-                <input className="inputs" type="number" placeholder="10"/>
-              </div>
-              <div className="section-child">
-                <label htmlFor="">Menor 2</label>
-                <input className="inputs" type="number" placeholder="10"/>
-              </div>
-            </div>
-            <div className="link">
-              <i className="fa fa-plus-circle fa-lg" aria-hidden="true"></i>
-              <a href="#">Añadir otra habitación</a>
-            </div>
-            <Grid.Row centered className='divButtonGreeen'>
-              <ButtonSearch typeSearch='hotel'/>
-            </Grid.Row>
+              <Grid.Row centered className='divButtonGreeen'>
+                <ButtonSearch typeSearch='hotel'/>
+              </Grid.Row>
+          </div>
+          </Grid>
         </div>
-        </Grid>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 FormSearchHotel.propTypes = {
