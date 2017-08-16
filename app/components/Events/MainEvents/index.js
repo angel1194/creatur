1a2ef1b63be514ea1615de3fd36471ddbedc2f11
Acopa123 <jose_acopa@hotmail.com>
2017-08-16 13:30:32 -0500

    Add rooms Manzanero

--- app/components/Events/MainEvents/index.js
+++ app/components/Events/MainEvents/index.js
@@ -7,22 +7,25 @@
 import React from 'react';
 import Tab from '../../Tab';
 import {Container, Grid, } from 'semantic-ui-react'
+import Hotels from '../Hotels';
+import Rooms from '../Rooms';
 // import styled from 'styled-components';
+
 class MainEvents extends React.Component {
   render() {
     return (
       <div>
-        <Container className= 'containerFormSearch'>
-          {/* <div className='ui container containerForm'> */}
-          <Tab
+        <Container>
+          {/* <Tab
             hotel='Hotel'
             auto='Hotel y Tickets'
             Vuelos="Transport"
             iconHotel='icon-hotel'
             iconCar='icon-car'
             iconVuelos='icon-plane'
-          />
-        {/* </div> */}
+          /> */}
+          {/* <Rooms/> */}
+          <Hotels/>
         </Container>
       </div>
     );
