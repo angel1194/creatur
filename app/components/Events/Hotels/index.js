ca86d20970a8ef4fd6c20767460a1710b3fe02f3
Acopa123 <jose_acopa@hotmail.com>
2017-08-17 16:13:35 -0500

    Add inicialStatet on Events component

--- app/components/Events/Hotels/index.js
+++ app/components/Events/Hotels/index.js
@@ -10,7 +10,9 @@ import PriceLabel from '../../ReserHotel/PriceLabel';
 import Button from '../../ReserHotel/Button';
 import OffersDay from '../../ReserHotel/OffersDay';
 import AddCheckList from '../../AddCheckList';
-import MapView from '../../ReserHotel/MapView'
+import MapView from '../../ReserHotel/MapView';
+import {Div, DivRooms} from './style';
+import InicialState from '../state'
 
 const ColumMarginRight = styled(Column) `
   margin-right: 10px;
@@ -24,44 +26,10 @@ const ColumMarginTop = styled(Column) `
   padding-left: 10px;
 `;
 
-const Div = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const DivRooms = styled.div`
-  width: 75%;
-`;
-
 class Hotels extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      hotels: {
-        '001':{
-          image:"http://cdn.precioyviajes.com/00/13/40/00134083_gp.jpg",
-          title:'Best Western Maya Yucatán',
-          price:'434.80',
-          numStart:4,
-          location:'Mérida',
-        },
-        '002':{
-          image:"https://images.bestday.com/_lib/vimages/Merida_Yucatan/Hotels/Hotel-Casa-Lucia/Fachada_t.jpg",
-          title:'Hotel Casa Lucía',
-          price:'2452',
-          numStart:4.5,
-          location:'Mérida - Zona Centro',
-        },
-        '003':{
-          image:"https://media-cdn.tripadvisor.com/media/photo-s/09/b9/7f/4a/hotel-residencial.jpg",
-          title:'Hotel Residencial',
-          price:'647',
-          numStart:3,
-          location:'Mérida',
-        }
-      }
-    }
+    this.state = InicialState
   }
   render() {
     const {hotels} = this.state
@@ -71,7 +39,7 @@ class Hotels extends React.Component {
         <Div>
           <DivRooms>
             {Object.keys(hotels).map((item, i)=>
-            <Column key={i}>
+            <Column>
               <Wrapper>
                 <Row>
                   <Img src={hotels[item].image} Width="200px" Height="156px" />
