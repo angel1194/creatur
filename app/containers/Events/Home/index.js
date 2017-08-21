29c28161a300bf7c473a899b567a63b00d646dc5
Acopa123 <jose_acopa@hotmail.com>
2017-08-21 17:58:54 -0500

    fix formHotel component

--- app/containers/Events/Home/index.js
+++ app/containers/Events/Home/index.js
@@ -3,26 +3,6 @@ import Hom from '../../../components/Events/Home';
 
 class Home extends Component {
 
-  request(event){
-    event.preventDefault()
-
-    let rooms = this.refs.rooms.value
-
-    let request = {
-      checkin:'',
-      checkout:'',
-      rooms:{
-        room:{
-          key:1,
-          adult:0,
-          child:{
-
-          }
-        },
-      }
-    }
-  }
-
   render() {
     return (
       <div>
