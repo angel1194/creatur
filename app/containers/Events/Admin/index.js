768e7e18834a06eed4c02680b34ec3bb5dbee0a5
Antonio Ortega <aortega@creatuviaje.com>
2017-08-21 11:08:25 -0500

    adding firebase to admin

--- app/containers/Events/Admin/index.js
+++ app/containers/Events/Admin/index.js
@@ -1,12 +1,48 @@
 import React,{Component} from 'react';
+import { browserHistory } from 'react-router';
+import styled from 'styled-components'
+import firebaseApp from '../Firebase'
 
+
+const Container =styled.div`
+  width:70%;
+  margin:0 auto;
+`;
 
 class Admin extends Component { // eslint-disable-line react/prefer-stateless-function
+
+  componentDidMount(){
+    let database=firebaseApp.database().ref();
+    database.on('value', (snap) => {
+        console.log(snap.val());
+      })
+    // firebaseApp.auth().onAuthStateChanged(user => {
+    //   if (user) {
+    //     console.log(user);
+    //     browserHistory.push('/manzanero/');
+    //   }
+    // });
+  }
+
+
+  logIn(){
+    var provider = new firebaseApp.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+      console.log(result);
+    })
+  }
+
+
   render() {
     return (
-      <div>
-        <h1>Hola admin</h1>
-      </div>
+      <Container>
+        <button onClick={this.logIn.bind(this)}>
+          Login with Google
+        </button>
+      </Container>
     );
   }
 }
