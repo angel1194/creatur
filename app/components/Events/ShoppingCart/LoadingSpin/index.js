b2590c475777af310d4f0d5ca7c33587233005ff
Acopa123 <jose_acopa@hotmail.com>
2017-10-20 12:58:05 -0500

    Fixe function FomrPayment

--- app/components/Events/ShoppingCart/LoadingSpin/index.js
+++ app/components/Events/ShoppingCart/LoadingSpin/index.js
@@ -14,7 +14,6 @@ function LoadingSpin(props) {
   return (
     <div>
       <div className='divSpin'>
-        {/*<i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>*/}
         <img src={ImgLoading}/>
         <span className="sr-only">Loading...</span>
       </div>
